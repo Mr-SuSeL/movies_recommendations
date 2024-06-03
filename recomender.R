d88e9711964965c281fbcc438e2913b80becc649
Łukasz Susłowicz <susel.invest@gmail.com>
2024-06-03 18:04:53 +0200

    Loading data and suitable CRAN packages

--- recomender.R
+++ recomender.R
@@ -3,3 +3,16 @@ library(ggplot2)
 library(tidyr)
 library(cluster)
 setwd("C:\Users\susel\OneDrive\Pulpit\UczenieMaszynowe_lab\Recomendations_Movies\movies_recomendations\recomender.R")
+
+ratings = read.csv("10K/ratings.csv", sep = ";") # user_id	movie_id	rating	timestamp
+movies = read.csv("10K/movies.csv", sep = ";") # movie_id	title	 genre[1-7]
+users = read.csv("10K/users.csv", sep = ";") # user_id	timestamp
+
+# deleting timestamp
+ratings <- ratings[,-4]
+
+#install.packages("reshape2", dependencies=TRUE)
+#install.packages("stringi", dependencies=TRUE)
+library(stringi)
+library(reshape2)
+
