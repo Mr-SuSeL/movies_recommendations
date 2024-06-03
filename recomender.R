6bf2eee1059efc597d8eeab6bcf8f32394d0acb5
Łukasz Susłowicz <susel.invest@gmail.com>
2024-06-03 19:10:08 +0200

    Recommender function just starts working well

--- recomender.R
+++ recomender.R
@@ -16,3 +16,34 @@ ratings <- ratings[,-4]
 library(stringi)
 library(reshape2)
 
+# Create ratings matrix with rows as users and columns as movies
+# ratingmat = dcast(ratings, user_id ~ movie_id, value.var = "rating", na.rm=FALSE)
+ratingmat = dcast(ratings, user_id ~ movie_id, value.var = "rating")
+
+# We can now remove user ids
+ratingmat = as.matrix(ratingmat[,-1]) #sparse matrix
+
+# let's choice to calculate similarity according to the cosine similarity method
+install.packages("recommenderlab", dependencies=TRUE)
+library(recommenderlab)
+
+# removing zeros
+#Convert ratings matrix to real rating matrix which makes it dense
+ratingmat = as(ratingmat, "realRatingMatrix") # broken
+
+#Normalize the ratings matrix
+ratingmat = normalize(ratingmat) # broken
+
+#Create Recommender Model. The parameters are UBCF and Cosine similarity. We take 10 nearest neighbours
+rec_mod = Recommender(ratingmat, method = "UBCF", param=list(method="Cosine",nn=10)) 
+
+
+
+
+
+
+
+
+
+
+
